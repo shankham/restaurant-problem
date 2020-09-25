93657f79e87219b74eb6fac80dd6ea4f969b8670
shankham <32229261+shankham@users.noreply.github.com>
2020-09-25 17:10:17 +0530

    Update RestaurantController.java

--- src/main/java/com/restaurant/demo/controller/RestaurantController.java
+++ src/main/java/com/restaurant/demo/controller/RestaurantController.java
@@ -1,8 +1,13 @@
 package com.restaurant.demo.controller;
 
+import com.restaurant.controller.ResponseEntity;
+import com.restaurant.demo.database.MenuDao;
+import com.restaurant.demo.database.OrderDao;
 import com.restaurant.demo.domain.FoodDelivery;
 import com.restaurant.demo.domain.Order;
 import com.restaurant.demo.service.RestaurantService;
+import com.restaurant.demo.util.ResponseWrapper;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -10,20 +15,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
 public class RestaurantController {
+	@Autowired
+    private final RestaurantService service;    
+    
+    private final Logger logger = LoggerFactory.getLogger(RestaurantController.class);
 
-    private final RestaurantService service;
+    
+  /*  @PostMapping(value = "/addOrder", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseEntity  FoodDelivery addOrderrder(final @RequestBody  Order order) {
+        return service.order(order);
+    }*/
 
-    public RestaurantController(RestaurantService service) {
-        this.service = service;
-    }
-
-    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody  FoodDelivery order(final @RequestBody  Order order) {
+    @PostMapping(value = "/addOrder", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseWrapper>  addOrder(final @RequestBody List<Map<String, String>> order) {
         return service.order(order);
     }
 
