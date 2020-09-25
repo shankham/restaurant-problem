f16561600c9f685c2b9087ed83e224144164faf7
shankham <32229261+shankham@users.noreply.github.com>
2020-09-25 17:54:52 +0530

    Create ResponseConstant.java

---
+++ src/main/java/com/restaurant/demo/domain/ResponseConstant.java
@@ -0,0 +1,9 @@
+package com.restaurant.demo.domain;
+
+
+public class ResponseConstant {
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAIL = "FAIL";
+    public static final String SUCCESS_CODE = "0";
+    public static final String FAIL_CODE = "1";
+}
