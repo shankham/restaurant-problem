28096c1a4ba1966b85f2d455d82506678b07585c
shankham <32229261+shankham@users.noreply.github.com>
2020-09-25 17:14:36 +0530

    Create Menu.java

---
+++ src/main/java/com/restaurant/demo/domain/Menu.java
@@ -0,0 +1,72 @@
+package com.restaurant.demo.domain;
+
+import java.util.ArrayList;
+
+
+public class Menu {
+    private String name;
+    private String description;
+    private String image;
+    private int price;
+    private String[] additional;
+
+    public Menu(){}
+
+    public Menu(String name, String description, String image, int price){
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String[] getAdditional() {
+        return additional;
+    }
+
+    public void setAdditional(String[] additional) {
+        this.additional = additional;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Menu [" +
+                "name=" + getName() +
+                "description=" + getDescription() +
+                "image=" + getImage() +
+                "price=" + getPrice() +
+                "additional=" + getAdditional() +
+                "]";
+    }
+}
